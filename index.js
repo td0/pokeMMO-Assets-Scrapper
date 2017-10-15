183c0ccec7cd22b4bed9fd328f7fcbac12195a37
td0 <tadho@me.com>
2017-10-16 06:18:32 +0700

    fixing accidentally deleted line, cuz I fcked everything
    Signed-off-by: td0 <tadho@me.com>

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ fs.readFile("./file_sources/list.xml", "utf8", (err,data) => {
   const totalFiles = Object.keys(xml.update_feed.file).length;
   let download_count = 0;
   let rFilesPath = [];
+  for( let x=0; x<totalFiles; x++) rFilesPath[x] = xml.update_feed.file[x].name;
   for( let c=download_from; c<totalFiles; c++) {
     let x = rFilesPath[c]; 
     const file_uri = "http://files.pokemmo.eu/download/client/"+x;
